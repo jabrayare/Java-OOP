6ba3191dd16543d7b733a5913fa9b15c9b05d964
jabrayare <jabra5980@gmail.com>
2021-09-28 23:39:25 -0400

    Library Booked modeled in OOP

--- Car.java
+++
@@ -1,8 +0,0 @@
-class Car {
-  private void Car(){
-
-  }
-  public void moveCar(){
-    System.out.println("Car moved");
-  }
-}
--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
 
 class Main {
   public static void main(String[] args) {
-    Car car = new Car();
-    car.moveCar();
+    Book1 book = new Book1("world's best book", "Ali");
+    Book1 book2 = new Book1("world's newest book", "Jibril");
+    Book1 book3 = new Book1("world's newest book 2021", "Kaka");
+    book.addBooks();
+    book2.addBooks();
+    book3.addBooks();
+    book3.checkout("world's best book");
+    System.out.println(book.printAllBooks());
   }
 }
